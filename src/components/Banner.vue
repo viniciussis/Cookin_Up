99574d52c35d2b94d817b1e1629a762fb3e1874e
Vinícius Silva Santos <vinicius01012@gmail.com>
2024-01-13 01:49:45 -0300

    Cookin Up first steps

---
+++ src/components/Banner.vue
@@ -0,0 +1,74 @@
+<template>
+  <header class="banner">
+    <div class="presentation">
+      <img src="../assets/images/logo.svg" alt="Cookin Up Logo" class="logo" />
+      <p class="header-lg para-header">
+        <span class="green-text">Um banquete de ideias para</span>
+        despertar o chef que há em você!
+      </p>
+      <p class="subtitle-lg">
+        Explore novas receitas todos os dias com os ingredientes que estão ao
+        seu alcance!
+      </p>
+    </div>
+    <img
+      src="../assets/images/banner-photo.png"
+      alt="Foto de uma mulher cozinhando com uma bacia de vidro nas mãos. Ela está sorrindo e está apoiando um celular entre seu rosto e ombro."
+      class="banner-photo"
+    />
+  </header>
+</template>
+
+<style scoped>
+.banner {
+  padding: 4rem 7.5rem;
+  color: var(--creme);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  column-gap: 3.25rem;
+}
+
+.logo {
+  height: 4.5rem;
+  margin-bottom: 3rem;
+}
+
+.para-header {
+  margin-bottom: 2rem;
+}
+
+.green-text {
+  color: var(--verde-medio, #3d6d4a);
+}
+
+.banner-photo {
+  width: 35rem;
+}
+
+@media only screen and (max-width: 1300px) {
+  .banner {
+    padding: 4rem 3.75rem;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .logo {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .banner {
+    padding: 4rem 1.5rem;
+  }
+
+  .banner-photo {
+    width: min(100%, 21.25rem);
+  }
+}
+</style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,87 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      ingredients: ['Alho', 'Manteiga', 'Cebola']
+    }
+  }
+}
+</script>
+
+<template>
+  <main class="main-content">
+    <span class="subtitle-lg my-list-text">
+
+    </span>
+    <ul class="my-list-ingredients">
+      <li v-for="ingredient in ingredients" class="ingredients">
+        {{ ingredient }}
+      </li>
+    </ul>
+  </main>
+</template>
+
+<style scoped>
+.main-content {
+  padding: 6.5rem 7.5rem;
+  border-radius: 3.75rem 3.75rem 0rem 0rem;
+  background: var(--creme, #FFFAF3);
+  color: var(--cinza, #444);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5rem;
+}
+
+.my-list-text {
+  color: var(--coral, #F0633C);
+  display: block;
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
+
+.my-list-ingredients {
+  display: flex;
+  justify-content: center;
+  gap: 1rem 1.5rem;
+  flex-wrap: wrap;
+}
+
+.ingredients {
+  display: inline-block;
+  border-radius: 0.5rem;
+  min-width: 4.25rem;
+  padding: 0.5rem;
+  text-align: center;
+  transition: 0.2s;
+  color: var(--creme, #FFFAF3);
+  background: var(--coral, #F0633C);
+  font-weight: 700;
+}
+
+.empty-list {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.25rem;
+
+  color: var(--coral, #F0633C);
+  text-align: center;
+}
+
+@media only screen and (max-width: 1300px) {
+  .main-content {
+    padding: 5rem 3.75rem;
+    gap: 3.5rem;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .main-content {
+    padding: 4rem 1.5rem;
+    gap: 4rem;
+  }
+}
+</style>
